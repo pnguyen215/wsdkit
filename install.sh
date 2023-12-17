857124859775aefb39ecd7ff12393e6413f999ff
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2023-12-17 20:41:14 +0700

    :sparkles: feat: added shell scripts #3

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@ echo "🚀 Installing wsdkit..."
 
 # Specify the GitHub repository owner and name
 owner="pnguyen215"
-repo="wsdkit"
+repository="wsdkit"
+mark="$repository-master"
 
 # Try fetching the latest release information using GitHub API
-release_info=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest")
+release_info=$(curl -s "https://api.github.com/repos/$owner/$repository/releases/latest")
 
 # Extract the download URL for the source code zip file
 zip_url=$(echo "$release_info" | grep -o '"browser_download_url": ".*wsdkit.*.zip"' | cut -d'"' -f4)
 
 if [ -z "$zip_url" ]; then
     echo "🚨 Latest release not found. Downloading from the master branch."
-    zip_url="https://github.com/$owner/$repo/archive/master.zip"
+    zip_url="https://github.com/$owner/$repository/archive/master.zip"
 fi
 
 # Specify the installation directory
@@ -22,21 +23,27 @@ install_dir="$HOME/wsdkit"
 # Download the repository as a zip file
 curl -LJO "$zip_url"
 
-# Extract the contents of the zip file
-unzip -o "wsdkit-master.zip" -d "$install_dir"
+# Extract the contents of the zip file directly into the install directory
+unzip -o "$mark.zip" -d "$install_dir"
+
+# Adjusted the directory structure to avoid nested wsdkit directory
+mv "$install_dir/$mark"/* "$install_dir/"
+
+# Remove the unnecessary nested wsdkit directory
+rmdir "$install_dir/$mark"
 
 # Add command to .zshrc if not already present
-command_to_add="source $install_dir/src/wsdkit.sh"
+line="source $install_dir/src/wsdkit.sh"
 zshrc="$HOME/.zshrc"
 
-if ! grep -q "$command_to_add" "$zshrc"; then
+if ! grep -q "$line" "$zshrc"; then
     echo "✅ Command added to .zshrc"
-    echo "$command_to_add" >>"$zshrc"
+    echo "$line" >>"$zshrc"
 else
     echo "🚨 Command already exists in .zshrc"
 fi
 
 # Clean up: remove the downloaded zip file
-rm "wsdkit-master.zip"
+rm "$mark.zip"
 
 echo "🍺 wsdkit has been successfully installed. Please restart your terminal or run 'source ~/.zshrc' to apply changes."
