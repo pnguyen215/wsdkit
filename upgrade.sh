39202c8702a722e622df4a2c3a405c7e98df313d
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2023-12-17 22:25:30 +0700

    :ok_hand: chore: refactor codebase #4

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
 echo "🚀 Upgrading wsdkit..."
 sudo rm -rf wsdkit
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/pnguyen215/wsdkit/master/install.sh)"
-source ~/.zshrc
+echo "🍺 wsdkit has been successfully upgraded. Please restart your terminal or run 'source ~/.zshrc' to apply changes."
