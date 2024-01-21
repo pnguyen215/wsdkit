12f8db8d5bca2a2d5f82d4c2656dca51b6093b64
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-01-21 18:56:42 +0700

    :recycle: refactor: updated CI giignores for angular/java/nodejs #4

--- src/devops/gitignore_ci.sh
+++ src/devops/gitignore_ci.sh
@@ -25,3 +25,87 @@ function gitignore_go_gen() {
     download_file ".gitignore" "https://raw.githubusercontent.com/pnguyen215/wsdkit.keys/master/gitignores/go_gitignore.txt"
 }
 alias gitignoregogen="gitignore_go_gen"
+
+# gitignore_java_gen function
+# Generate a .gitignore file tailored for Java projects.
+#
+# Usage:
+#   gitignore_java_gen
+#
+# Description:
+#   The 'gitignore_java_gen' function downloads a .gitignore file specifically designed for Java projects
+#   and saves it to the specified location.
+#
+# Options:
+#   None
+#
+# Example usage:
+#   gitignore_java_gen
+#
+# Instructions:
+#   1. Run 'gitignore_java_gen' to download the .gitignore file tailored for Java projects.
+#   2. The .gitignore file will be saved as '.gitignore' in the project root directory.
+#
+# Notes:
+#   - Ensure that 'curl' is installed for proper functionality.
+#   - The URL points to the specific 'java_gitignore.txt' file in the 'wsdkit.keys' repository on GitHub.
+function gitignore_java_gen() {
+    download_file ".gitignore" "https://raw.githubusercontent.com/pnguyen215/wsdkit.keys/master/gitignores/java_gitignore.txt"
+}
+alias gitignorejavagen="gitignore_java_gen"
+
+# gitignore_angular_gen function
+# Generate a .gitignore file tailored for Angular projects.
+#
+# Usage:
+#   gitignore_angular_gen
+#
+# Description:
+#   The 'gitignore_angular_gen' function downloads a .gitignore file specifically designed for Angular projects
+#   and saves it to the specified location.
+#
+# Options:
+#   None
+#
+# Example usage:
+#   gitignore_angular_gen
+#
+# Instructions:
+#   1. Run 'gitignore_angular_gen' to download the .gitignore file tailored for Angular projects.
+#   2. The .gitignore file will be saved as '.gitignore' in the project root directory.
+#
+# Notes:
+#   - Ensure that 'curl' is installed for proper functionality.
+#   - The URL points to the specific 'angular_gitignore.txt' file in the 'wsdkit.keys' repository on GitHub.
+function gitignore_angular_gen() {
+    download_file ".gitignore" "https://raw.githubusercontent.com/pnguyen215/wsdkit.keys/master/gitignores/angular_gitignore.txt"
+}
+alias gitignoreangulargen="gitignore_angular_gen"
+
+# gitignore_nodejs_gen function
+# Generate a .gitignore file tailored for Node.js projects.
+#
+# Usage:
+#   gitignore_nodejs_gen
+#
+# Description:
+#   The 'gitignore_nodejs_gen' function downloads a .gitignore file specifically designed for Node.js projects
+#   and saves it to the specified location.
+#
+# Options:
+#   None
+#
+# Example usage:
+#   gitignore_nodejs_gen
+#
+# Instructions:
+#   1. Run 'gitignore_nodejs_gen' to download the .gitignore file tailored for Node.js projects.
+#   2. The .gitignore file will be saved as '.gitignore' in the project root directory.
+#
+# Notes:
+#   - Ensure that 'curl' is installed for proper functionality.
+#   - The URL points to the specific 'node.js_gitignore.txt' file in the 'wsdkit.keys' repository on GitHub.
+function gitignore_nodejs_gen() {
+    download_file ".gitignore" "https://raw.githubusercontent.com/pnguyen215/wsdkit.keys/master/gitignores/node.js_gitignore.txt"
+}
+alias gitignorenodejsgen="gitignore_nodejs_gen"
