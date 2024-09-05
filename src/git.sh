2269de88d010ab5e4f4b0fa9f04e2039ff212c1b
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-09-05 22:10:35 +0700

    :zap: improvement: change message when committing message #4

--- src/git.sh
+++ src/git.sh
@@ -1001,8 +1001,9 @@ function git_commit_with_format() {
         local commit_hash=$(git rev-parse HEAD)
         local repository_path=$(git rev-parse --show-toplevel)
         local repository_name=$(basename "$repository_path")
+        local server_remote_url=$(git config --get remote.origin.url)
         local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-        send_telegram_git_activity "🚀 *AI Workflow Administrator* \n - *username*: *$git_username* \n - *repository*: $repository_name\n - *branch*: \`$current_branch\` \n - *hash*: \`$commit_hash\` \n - *message*: \`$commit_message\` \n\n - *timestamp*: \`$timestamp\`"
+        send_telegram_git_activity "🚀 *AI Workflow Administrator* \n - *username*: *$git_username* \n - *repository*: [$repository_name]($server_remote_url)\n - *branch*: \`$current_branch\` \n - *hash*: \`$commit_hash\` \n - *message*: \`$commit_message\` \n\n ⏰ *timestamp*: \`$timestamp\`"
 
         wsd_exe_cmd git push -f
         echo "🍺 The commit pushed successfully to origin."
