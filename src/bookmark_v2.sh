6fcd2d65d8ef0094e3d2faefc90ca491aed71bf9
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-09-05 11:43:08 +0700

    :mute: silent changes: add unify functions support bookmark v2 #17

--- src/bookmark_v2.sh
+++ src/bookmark_v2.sh
@@ -1,3 +1,16 @@
+# uplink function
+# Creates a hard link between the specified source and destination.
+#
+# Usage:
+#   uplink <source name> <destination name>
+#
+# Description:
+#   The 'uplink' function creates a hard link between the specified source file and destination file.
+#   This allows multiple file names to refer to the same file content.
+#
+# Dependencies:
+#   - The 'ln' command for creating hard links.
+#   - The 'chmod' command to modify file permissions.
 uplink() {
     #T0DO : uplink <source name> <destination name> -- makes hard link
     #/usr/local/bin/goto
@@ -17,6 +30,18 @@ uplink() {
     fi
 }
 
+# cecho function
+# Prints colored text to the terminal.
+#
+# Usage:
+#   cecho <text> <color code>
+#
+# Description:
+#   The 'cecho' function outputs text in the terminal with the specified color.
+#   Color codes are optional and default to blue if not specified.
+#
+# Dependencies:
+#   - The 'tput' command for terminal control.
 cecho() {
     arg=$1
     arg2=$2
@@ -62,6 +87,18 @@ cecho() {
     echo $bold$color$arg$reset
 }
 
+# opent function
+# Opens the specified directory in a new Finder tab (Mac OS only).
+#
+# Usage:
+#   opent [directory]
+#
+# Description:
+#   The 'opent' function opens the specified directory in a new Finder tab on Mac OS.
+#   If no directory is specified, it opens the current directory.
+#
+# Dependencies:
+#   - The 'osascript' command for AppleScript support.
 opent() {
     # T0DO : open directories inside : opent bin --> opne ./bin
     if [ $# -eq 0 ]; then
@@ -82,6 +119,15 @@ if [[ ! -f $bookmarks_file ]]; then
     touch $bookmarks_file
 fi
 
+# bookmark function
+# Adds a bookmark for the current directory with the specified name.
+#
+# Usage:
+#   bookmark <bookmark name>
+#
+# Description:
+#   The 'bookmark' function creates a bookmark for the current directory with the given name.
+#   It allows quick navigation to the specified directory using the bookmark name.
 bookmark() {
     bookmark_name=$1
 
@@ -114,7 +160,15 @@ bookmark() {
     fi
 }
 
-# Delete the named bookmark from the list
+# deletemark function
+# Deletes a bookmark with the specified name from the bookmarks list.
+#
+# Usage:
+#   deletemark <bookmark name>
+#
+# Description:
+#   The 'deletemark' function removes a bookmark from the list based on the provided bookmark name.
+#   It prompts for confirmation before deleting the bookmark.
 deletemark() {
     bookmark_name=$1
 
@@ -132,7 +186,15 @@ deletemark() {
     fi
 }
 
-# Show a list of the bookmarks
+# showmarks function
+# Displays a formatted list of all bookmarks.
+#
+# Usage:
+#   showmarks
+#
+# Description:
+#   The 'showmarks' function lists all bookmarks in a formatted manner, showing the bookmark name and its associated directory.
+#   It uses color coding to enhance readability.
 showmarks() {
     yellow=$(tput setaf 3)
     normal=$(tput sgr0)
@@ -140,6 +202,14 @@ showmarks() {
     #cat $bookmarks_file | awk '{ printf "%-40s%-40s%s\n",$1,$2,$3}' FS=\|
 }
 
+# gotodir function
+# Navigates to the directory associated with the specified bookmark name.
+#
+# Usage:
+#   gotodir <bookmark name>
+#
+# Description:
+#   The 'gotodir' function changes the current working directory to the location
 gotodir() {
     bookmark_name=$1
     bookmark=$(grep "|$bookmark_name$" "$bookmarks_file")
@@ -152,15 +222,39 @@ gotodir() {
     fi
 }
 
+# goback function
+# Navigates to the previous working directory.
+#
+# Usage:
+#   goback
+#
+# Description:
+#   The 'goback' function changes the current working directory to the previous directory in the history.
 goback() {
     cd $OLDPWD
 }
 
+# cpydir function
+# Copies the current directory path to the clipboard.
+#
+# Usage:
+#   cpydir
+#
+# Description:
+#   The 'cpydir' function copies the current directory path to the clipboard using the 'pbcopy' command.
 cpydir() {
     adr=$PWD
     echo -n $adr | pbcopy
 }
 
+# goto function
+# Main function to handle user commands and navigate directories.
+#
+# Usage:
+#   goto [command]
+#
+# Description:
+#   The 'goto' function processes user commands to navigate directories, manage bookmarks,
 goto() {
     if [ $# -eq 0 ]; then
         showmarks
@@ -210,10 +304,24 @@ goto() {
 
 }
 
+# goto_version function
+# Displays the version of the goto script.
+#
+# Usage:
+#   goto_version
+#
+# Description:
 goto_version() {
     echo "goto v0.0.1"
 }
 
+# goto_help function
+# Displays the help information for the goto script.
+#
+# Usage:
+#   goto_help
+#
+# Description:
 goto_help() {
     echo "  USAGE:"
     echo
